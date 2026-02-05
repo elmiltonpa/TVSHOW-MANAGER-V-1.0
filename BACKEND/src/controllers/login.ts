f533673f8f1e5305533f7362cddcced5a5b09462
Milton Ibarra <elmiltonibarra@gmail.com>
2026-02-05 15:55:02 -0300

    increase JWT expiration to 30 days

--- BACKEND/src/controllers/login.ts
+++ BACKEND/src/controllers/login.ts
@@ -6,36 +6,43 @@ import config from "../config.js";
 
 const loginRouter = Router();
 
-loginRouter.post("/", async (request: Request, response: Response, next: NextFunction) => {
-  const { username, password } = request.body;
+loginRouter.post(
+  "/",
+  async (request: Request, response: Response, next: NextFunction) => {
+    const { username, password } = request.body;
 
-  try {
-    const user = await User.findOne({ username });
+    try {
+      const user = await User.findOne({ username });
 
-    const passwordCorrect =
-      user === null ? false : await bcrypt.compare(password, user.passwordHash);
+      const passwordCorrect =
+        user === null
+          ? false
+          : await bcrypt.compare(password, user.passwordHash);
 
-    if (!(user && passwordCorrect)) {
-      return response.status(401).json({
-        error: "invalid user or password",
+      if (!(user && passwordCorrect)) {
+        return response.status(401).json({
+          error: "invalid user or password",
+        });
+      }
+
+      const userForToken = {
+        id: user._id,
+        username: user.username,
+      };
+
+      const token = jwt.sign(userForToken, config.JWT_SECRET, {
+        expiresIn: "30d",
       });
+
+      response.status(200).send({
+        name: user.name,
+        username: user.username,
+        token,
+      });
+    } catch (error) {
+      next(error);
     }
-
-    const userForToken = {
-      id: user._id,
-      username: user.username,
-    };
-
-    const token = jwt.sign(userForToken, config.JWT_SECRET, { expiresIn: "24h" });
-
-    response.status(200).send({
-      name: user.name,
-      username: user.username,
-      token,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+  },
+);
 
 export default loginRouter;
